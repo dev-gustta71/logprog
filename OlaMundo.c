6e787c75a4fcdfad8b6d3f5b1b3b40010f592de0
Gustavo Almeida <128623455+dev-gustta71@users.noreply.github.com>
2023-05-16 16:26:04 -0300

    Add files via upload

---
+++ OlaMundo.c
@@ -0,0 +1,29 @@
+#include <stdio.h> // entrada no teclado
+
+int main()
+{ // inicio
+
+   // variaveis
+   // int = inteiro
+   // float = real
+   // char = caractere
+   // char [50] = caractere
+
+   // IGUAL ==
+   // DIFERENTE !=
+   // %d = inteiro
+   // %f = float
+   // %s = caractere
+   // %c = uma caracter
+
+   int idade = 19;
+   float peso = 79;
+   char nome[10] = "GUSTAVO";
+   char sexo = 'M';
+
+   printf("IDADE: %d \n", idade); // escreval
+   printf("PESO: %.2f \n", peso);
+   printf("NOME: %s \n", nome);
+   printf("SEXO %c \n", sexo);
+   return 0;
+} // fimalgoritmo
